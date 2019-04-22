b7b73afe779eb7d2c1dbca779d0c1caa8997227c
Jonathas Ribeiro <contact@jonathas.com>
2019-04-22 16:40:44 +0200

    Initial commit

---
+++ app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,19 @@
+<?php namespace estoque\Http\Controllers;
+
+use Illuminate\Support\Facades\DB;
+
+class ProdutoController extends Controller {
+
+    public function lista() {
+        $html = '<h1>Listagem de produtos</h1>';
+
+        $produtos = DB::select('select * from produtos');
+
+        foreach($produtos as $p) {
+            $html .= '<br/> Nome: ' . $p->nome;
+        }
+
+        return $html;
+    }
+
+}
