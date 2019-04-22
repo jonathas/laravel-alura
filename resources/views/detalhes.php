2431c168b2fe65ea471f14db1a6ec641aa2728ee
Jonathas Ribeiro <contact@jonathas.com>
2019-04-22 21:35:08 +0200

    Added more items to the details page

--- resources/views/detalhes.php
+++ resources/views/detalhes.php
@@ -13,7 +13,15 @@
     <div class="container">
         <h1>Detalhes do produto <?= $p->nome ?></h1>
         <ul>
-            <li>Descrição: <?= $p->descricao ?></li>
+            <li>
+                <b>Valor:</b> R$ <?= $p->valor ?>
+            </li>
+            <li>
+                <b>Descrição:</b> <?= $p->descricao ?>
+            </li>
+            <li>
+                <b>Quantidade em estoque:</b> <?= $p->quantidade ?>
+            </li>
         </ul>
     </div>
 </body>
